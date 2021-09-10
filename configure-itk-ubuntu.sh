0881221490c92bce2aa964d5d32b5199f458699c
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-09-10 09:26:04 +0100

    Build VMTK with VTK 9 and ITK 5. Update python versions to all supported

--- configure-itk-ubuntu.sh
+++ configure-itk-ubuntu.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+. $thisdir/cmake_help.sh
+ 
+declare -A D
 
-cmake \
-    -B build \
-    -S itk \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
-    -DBUILD_EXAMPLES:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DITK_USE_REVIEW:BOOL=ON \
-    -DITK_USE_REVIEW_STATISTICS:BOOL=ON \
-    -DITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON \
-    -DCMAKE_BUILD_WITH_INSTALL_RPATH=OFF
+D[CMAKE_INSTALL_PREFIX]=$INSTALL_PREFIX
+D[CMAKE_BUILD_TYPE]=Release
+D[BUILD_SHARED_LIBS]=ON
+D[BUILD_EXAMPLES]=OFF
+D[BUILD_TESTING]=OFF
+D[CMAKE_BUILD_WITH_INSTALL_RPATH]=OFF
+D[CMAKE_INSTALL_RPATH_USE_LINK_PATH]=TRUE
+
+# From Superbuild ITK 5
+D[ITK_LEGACY_REMOVE]=OFF   #<-- Allow LEGACY ITKv4 features for now.
+D[ITK_LEGACY_SILENT]=ON   #<-- Use of legacy code will produce compiler warnings
+D[Module_ITKDeprecated]=ON #<-- Needed for ITKv5 now. (itkMultiThreader.h and MutexLock backwards compatibility.)
+D[Module_ITKReview]=OFF
+
+mkdir -p build
+cd build
+
+run_cmake D ../itk
