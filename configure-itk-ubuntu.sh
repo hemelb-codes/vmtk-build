12cf431b300add3a5ba032641b653cadb4da8520
Rupert Nash <r.nash@epcc.ed.ac.uk>
2021-11-30 16:40:13 +0000

    alter ITK build opts

--- configure-itk-ubuntu.sh
+++ configure-itk-ubuntu.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+thisdir=$(readlink -f $(dirname $BASH_SOURCE))
+. $thisdir/cmake_help.sh
+ 
+declare -A D
 
-cmake \
-    -B build \
-    -S itk \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
-    -DBUILD_EXAMPLES:BOOL=OFF \
-    -DBUILD_TESTING:BOOL=OFF \
-    -DITK_USE_REVIEW:BOOL=ON \
-    -DITK_USE_REVIEW_STATISTICS:BOOL=ON \
-    -DITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON \
-    -DCMAKE_BUILD_WITH_INSTALL_RPATH=OFF
+D[CMAKE_INSTALL_PREFIX]=$INSTALL_PREFIX
+D[CMAKE_BUILD_TYPE]=Release
+D[BUILD_SHARED_LIBS]=ON
+D[BUILD_EXAMPLES]=OFF
+D[BUILD_TESTING]=OFF
+D[CMAKE_BUILD_WITH_INSTALL_RPATH]=OFF
+D[CMAKE_INSTALL_RPATH_USE_LINK_PATH]=TRUE
+
+if [[ $ITK_VERSION =~ '5\..*' ]]; then
+    # From Superbuild ITK 5
+    D[ITK_LEGACY_REMOVE]=OFF   #<-- Allow LEGACY ITKv4 features for now.
+    D[ITK_LEGACY_SILENT]=OFF   #<-- Use of legacy code will produce compiler warnings
+    D[Module_ITKDeprecated]=ON #<-- Needed for ITKv5 now. (itkMultiThreader.h and MutexLock backwards compatibility.)
+    D[Module_ITKReview]=OFF
+else
+    # Probably 4
+    D[ITK_LEGACY_REMOVE]=ON
+    D[Module_ITKDeprecated]=OFF
+    D[Module_ITKReview]=ON
+fi
+
+mkdir -p build
+cd build
+
+run_cmake D ../itk
